7868dbb62c9ce8e30ec1576b53b6ef49487aff2e
René Olivo <minet.ws@gmail.com>
2018-02-06 20:50:17 -0400

    Add webpack compilation for ES6, Sass, and Pug

---
+++ client/webpack.config.js
@@ -0,0 +1,87 @@
+'use strict';
+
+const webpack = require('webpack');
+const Extract = require('extract-text-webpack-plugin');
+const path = require('path');
+
+const SRC_DIR = path.join(__dirname, 'src');
+const DIST_DIR = path.join(__dirname, 'public/dist');
+
+const extractCss = new Extract('styles.css');
+let cssLoader;
+let sourcemap;
+let plugins = [
+  extractCss
+  // new webpack.NoEmitOnErrorsPlugin(),
+];
+
+if (process.env.ENV === 'production') {
+  cssLoader = extractCss.extract([
+    'css-loader',
+    'sass-loader'
+  ]);
+  sourcemap = 'source-map';
+  plugins.push(new webpack.optimize.UglifyJsPlugin({
+    compress: { warnings: false }
+  }));
+} else {
+  cssLoader = 'style-loader!css-loader!sass-loader';
+  sourcemap = 'inline-source-map';
+}
+
+module.exports = {
+  entry: path.join(SRC_DIR, 'app.js'),
+  output: {
+    path: DIST_DIR,
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.scss$/,
+        loader: cssLoader,
+        exclude: /node_modules/
+      },
+      {
+        test: /\.(ttf|eot|woff2?)(\?v=.*)?$/,
+        loader: 'file-loader',
+        query: {
+          name: '../fonts/[name].[ext]'
+        }
+      },
+      {
+        test: /fonts\/.*\.svg(\?v=.*)?$/,
+        loader: 'file-loader',
+        query: {
+          name: '../fonts/[name].[ext]'
+        }
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif)$/,
+        loader: 'file-loader',
+        query: {
+          name: '../images/[name].[ext]'
+        }
+      },
+      {
+        test: /\.pug$/,
+        loader: 'pug-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: require.resolve('jquery'),
+        loader: 'expose-loader?jQuery!expose-loader?$'
+      }
+    ]
+  },
+  plugins: plugins,
+  stats: {
+    colors: true
+  },
+  devtool: sourcemap
+};
--- server/app.js
+++ server/app.js
@@ -6,7 +6,7 @@ const express = require('express');
 const app = express();
 const db = require('../models');
 
-app.use(express.static('public'));
+app.use(express.static('client/public'));
 
 app.get('/api/users', function (req, res) {
   db.users.findAll()
