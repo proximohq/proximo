a36c281a1b9c9c891c57536e7773a5367d214bcc
René Olivo <minet.ws@gmail.com>
2018-02-24 04:04:52 +0530

    Return JWT token on successful login

--- server/src/routes/api/auth/login.js
+++ server/src/routes/api/auth/login.js
@@ -1,34 +1,117 @@
+const fs = require('fs');
+const jwt = require('jsonwebtoken');
+
 const db = require.main.require('../models');
 const security = require.main.require('./lib/security');
 
 module.exports = function (req, res) {
-  if (!req.body.email || !req.body.password) {
-    return res.status(500).send({
+  let data, token, user;
+
+  validateRequest()
+    .then(findUserForRequestEmail)
+    .then(checkIfPasswordMatches)
+    .then(prepareToken)
+    .then(respondSuccessfully)
+    .catch((error) => {
+      res.status(500).send({ message: 'Internval Server Error' });
+      console.error(error);
+    });
+
+  /**
+   * Validates that the request contains the email and password fields, otherwise
+   * rejects the request.
+   *
+   * @return {Promise}
+   */
+  function validateRequest () {
+    if (req.body.email && req.body.password) {
+      return Promise.resolve();
+    }
+
+    res.status(500).send({
       error: true,
       message: 'Must provide "email" and "password" fields'
     });
+
+    return Promise.reject();
   }
 
-  db.users.find({ where: { email: req.body.email } })
-    .then(function (result) {
-      if (result === null) {
-        return res.status(404).send({
+  /**
+   * Finds the user for the specified email address. If the user is not found
+   * the server responds with a 404.
+   *
+   * @return {Promise}
+   */
+  function findUserForRequestEmail () {
+    return db.users.find({ where: { email: req.body.email } })
+      .then(function (result) {
+        user = result;
+
+        if (user !== null) {
+          return user;
+        }
+
+        res.status(404).send({
           error: true,
           message: `User with email ${req.body.email} not found`
         });
-      }
 
-      const [salt, hashedPassword] = result.password.toString().split(':::');
+        return Promise.reject();
+      });
+  }
 
-      const hash = security.hash(req.body.password, salt);
+  /**
+   * Determines if the password provided matches the one stored in the DB.
+   * To do this, the password is encrypted using the salt stored in the DB.
+   *
+   * If the password don't match the server rejects the request.
+   *
+   * @return {Promise}
+   */
+  function checkIfPasswordMatches () {
+    const [salt, hashedPassword] = user.password.toString().split(':::');
+    const hash = security.hash(req.body.password, salt);
 
-      if (hash === hashedPassword) {
-        res.send({success: true});
-      } else {
-        res.status(500).send({
-          error: true,
-          message: `Wrong password`
-        });
-      }
+    if (hash === hashedPassword) {
+      return;
+    }
+
+    res.status(500).send({
+      error: true,
+      message: `Wrong password`
     });
+
+    return Promise.reject();
+  }
+
+  /**
+   * Signs the user data using the private certificate.
+   */
+  function prepareToken () {
+    let certificate = fs.readFileSync('private/private.key');
+
+    data = {
+      user: {
+        id: user.id,
+        firstName: user.firstName,
+        lastName: user.lastName,
+        email: user.email
+      }
+    };
+
+    token = jwt.sign(data, certificate, {
+      expiresIn: '1h'
+    });
+  }
+
+  /**
+   * Responds the request with the JWT signed token and the user data.
+   */
+  function respondSuccessfully () {
+    res.send({
+      success: true,
+      data: data,
+      token: token
+    });
+  }
 };
